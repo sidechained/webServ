6a7346e7227a0222d94fbd6dabb345a0aadd5256
Graham Booth <graham.r.booth@gmail.com>
2023-09-20 12:14:00 +0200

    parsing multipart file body in progress

--- postUploadRequestParser/PostUploadRequestParser.cpp
+++ postUploadRequestParser/PostUploadRequestParser.cpp
@@ -2,6 +2,14 @@
 
 PostUploadRequestParser::PostUploadRequestParser()
 {
+	// init accepted field for header
+	headerAcceptedFields.push_back("host");
+	headerAcceptedFields.push_back("content-type");
+	headerAcceptedFields.push_back("content-length");
+	// init accepted fields for parts (multipart)
+	partsAcceptedFields.push_back("content-disposition");
+	partsAcceptedFields.push_back("content-type");
+	// 
 	std::fstream requestFile("postRequest.txt");
 	if(!requestFile.is_open()) {
 		std::cout << "Could not open file" << std::endl;
@@ -11,15 +19,13 @@ PostUploadRequestParser::PostUploadRequestParser()
 	getline(requestFile, line);	
 	HttpRequest httpRequest;
 	parseHttpRequest(line, httpRequest);
-	if (!httpRequest.method.empty()) {
-		std::cout << "Method: " << httpRequest.method << std::endl;
-		std::cout << "Resource: " << httpRequest.resource << std::endl;
-		std::cout << "HTTP Version: " << httpRequest.httpVersion << std::endl;
-	}
-	parseHeaderBlock(requestFile, line);
+	// printHttpRequest(httpRequest);
+	parseHeaderBlock(requestFile, line, headerMap, headerAcceptedFields);
+	// printMap();
+	parseContentTypeValue();
+	parseBody(requestFile);
 	requestFile.close();
 }
-
 
 PostUploadRequestParser::~PostUploadRequestParser()
 {
@@ -61,49 +67,194 @@ void PostUploadRequestParser::parseHttpRequest(const std::string& line, HttpRequ
 	}
 }
 
-bool PostUploadRequestParser::isAcceptedField(std::string field) {
-	std::vector<std::string> acceptedFields;
-	acceptedFields.push_back("Host");
-	acceptedFields.push_back("Content-Type");
-	acceptedFields.push_back("Content-Length");
+bool PostUploadRequestParser::isAcceptedField(std::string field, std::vector<std::string> acceptedFields) {
 	if (std::find(acceptedFields.begin(), acceptedFields.end(), field) != acceptedFields.end())
 		return true;
 	return false;
 }
 
-void PostUploadRequestParser::parseHeaderLine(const std::string& line, std::map<std::string, std::string>& headerMap) {
+void PostUploadRequestParser::parseHeaderLine(const std::string& line, std::map<std::string, std::string> &headerMap, std::vector<std::string> acceptedFields) {
 	std::string::size_type colonPos = line.find(':');
 	if (colonPos != std::string::npos) {
 		std::string field = line.substr(0, colonPos);
+		toLowerCase(field); // headers are case insensitive, so convert to and deal with in lower case only
 		std::string value = line.substr(colonPos + 1);
-		if(isAcceptedField(field))
+		if(isAcceptedField(field, acceptedFields))
 		{
 			// Remove leading whitespaces from the value
 			std::string::size_type valueStart = value.find_first_not_of(" \t");
 			if (valueStart != std::string::npos)
 				value = value.substr(valueStart);
-			
 			// Store the field and value in the map
 			headerMap[field] = value;
 		}
 	}
 }
 
-void PostUploadRequestParser::parseHeaderBlock(std::fstream &requestFile, std::string &line)
+void PostUploadRequestParser::parseHeaderBlock(std::fstream &requestFile, std::string &line, std::map<std::string, std::string> &headerMap, std::vector<std::string> acceptedFields)
 {
-	std::map<std::string, std::string> headerMap;
 	while(!line.empty())
 	{
 		getline(requestFile, line);
-		parseHeaderLine(line, headerMap);
+		parseHeaderLine(line, headerMap, acceptedFields);
 	}
-	printHeaderMap(headerMap);
 }
 
-void PostUploadRequestParser::printHeaderMap(std::map<std::string, std::string> headerMap) {
+void PostUploadRequestParser::parseHeaderBlockFromString(std::string& inputString, std::string& line, std::map<std::string, std::string>& headerMap, std::vector<std::string> acceptedFields)
+{
+    std::istringstream iss(inputString);  // Treat the input string as a stream
+    // Read lines from the stringstream
+    while (std::getline(iss, line) && !line.empty()) {
+        parseHeaderLine(line, headerMap, acceptedFields);
+		removeFromStringStart(inputString, line);
+    }
+}
+
+void PostUploadRequestParser::parseContentTypeValue()
+{
+	std::string contentType;
+	std::vector<std::string> tokens;
+
+	// 1. Split the "Content-Type" header value by the semicolon (;)
+	splitString(headerMap["content-type"], ";", tokens);
+	if (tokens.size() != 2)
+	{
+		std::cout << "syntax error1!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	// 2. Trim any leading or trailing whitespace from each part.
+	for(size_t i = 0; i < tokens.size(); i++)
+	{
+		trimWhitespace(tokens[i]);
+	}
+	// 3. Check each part to find the one that starts with "boundary=". This part contains the boundary string.
+	if(startsWith(tokens[0], "boundary=")) {
+		boundary = tokens[0];
+		contentType = tokens[1];
+	} 
+	else if(startsWith(tokens[1], "boundary=")) {
+		boundary = tokens[1];
+		contentType = tokens[0];
+	}
+	else {
+		std::cout << "syntax error2!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	// 4. Extract the boundary string by splitting the part that starts with "boundary=" and retrieves the value after the equal sign (=).	
+	removeFromStringStart(boundary, "boundary=");
+	// 5. Check if content type matches "multipart/form-data"
+	if (contentType != "multipart/form-data") {
+		std::cout << "syntax error3!" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
+void PostUploadRequestParser::parseBody(std::fstream &requestFile)
+{
+	std::string endBoundary = "--" + boundary + "--";
+	// look for end Boundary and erase it and everything after, treating this as our body
+	std::string body;
+	body.assign(std::istreambuf_iterator<char>(requestFile), std::istreambuf_iterator<char>());
+	size_t foundPos = body.find(endBoundary);
+	if (!(foundPos != std::string::npos)) {
+		std::cout << "End boundary not found.\n";
+		exit(EXIT_FAILURE);
+	}
+	body.erase(foundPos);
+	// split the new body by the boundary string
+	std::string splitBoundary = "--" + boundary;
+	boundary = "--" + boundary + '\n';
+	std::vector<std::string> parts;
+	splitString(body, boundary, parts);
+	std::map<std::string, std::string> partMap;
+	for(size_t i = 0; i < parts.size(); i++)
+	{
+		std::string line;
+		parseHeaderBlockFromString(parts[i], line, partMap, partsAcceptedFields);
+		std::cout << parts[i] << std::endl;
+		printMap(partMap);
+	}
+}
+
+// utility functions
+
+void PostUploadRequestParser::toLowerCase(std::string& str) {
+	for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
+		*it = std::tolower(*it);
+	}
+}
+
+void PostUploadRequestParser::splitString(const std::string& input, const std::string& delimiter, std::vector<std::string>& output) {
+	std::string::size_type start = 0;
+	std::string::size_type end = input.find(delimiter);
+
+	while (end != std::string::npos) {
+		// Only add non-empty substrings to the output
+		if (start != end)
+			output.push_back(input.substr(start, end - start));
+
+		start = end + delimiter.length();  // Move start to the beginning of the next substring
+		end = input.find(delimiter, start);
+	}
+
+	// Add the last token if it's non-empty
+	if (start < input.length()) {
+		std::string lastToken = input.substr(start);
+		if (!lastToken.empty())
+			output.push_back(lastToken);
+	}
+}
+
+void PostUploadRequestParser::trimWhitespace(std::string& str) {
+	// Trim leading whitespace
+	std::string::size_type start = str.find_first_not_of(" \t\n\r");
+	if (start != std::string::npos) {
+		str.erase(0, start);
+	} else {
+		str.clear();  // String contains only whitespace
+		return;
+	}
+	// Trim trailing whitespace
+	std::string::size_type end = str.find_last_not_of(" \t\n\r");
+	if (end != std::string::npos) {
+		str.erase(end + 1);
+	}
+}
+
+bool PostUploadRequestParser::startsWith(const std::string& fullString, const std::string& start) {
+	if (fullString.length() < start.length())
+		return false;
+	for (std::string::size_type i = 0; i < start.length(); ++i) {
+		if (fullString[i] != start[i])
+			return false;
+	}
+	return true;
+}
+
+void PostUploadRequestParser::removeFromStringStart(std::string& fullString, const std::string& line) {
+    std::string::size_type foundPos = fullString.find(line);
+
+    if (foundPos == 0) {
+        // Line found at the start, erase it
+        fullString.erase(0, line.length());
+
+        // Additionally, remove any leading whitespace or newline
+        while (!fullString.empty() && (fullString[0] == ' ' || fullString[0] == '\t' || fullString[0] == '\n' || fullString[0] == '\r'))
+            fullString.erase(0, 1);
+    }
+}
+
+void PostUploadRequestParser::printHttpRequest(HttpRequest& httpRequest) {
+	if (!httpRequest.method.empty()) {
+		std::cout << "Method: " << httpRequest.method << std::endl;
+		std::cout << "Resource: " << httpRequest.resource << std::endl;
+		std::cout << "HTTP Version: " << httpRequest.httpVersion << std::endl;
+	}
+}	
+
+void PostUploadRequestParser::printMap(std::map<std::string, std::string> map) {
 	std::map<std::string, std::string>::iterator it;
-	for (it = headerMap.begin(); it != headerMap.end(); ++it) {
+	for (it = map.begin(); it != map.end(); ++it) {
 		std::cout << "Field: " << it->first << ", Value: " << it->second << std::endl;
 	}
 }
-
--- postUploadRequestParser/PostUploadRequestParser.hpp
+++ postUploadRequestParser/PostUploadRequestParser.hpp
@@ -3,6 +3,7 @@
 
 # include <cstdlib>
 # include <fstream>
+# include <sstream>
 # include <iostream>
 # include <string>
 # include <map>
@@ -21,15 +22,28 @@ class PostUploadRequestParser
 			std::string accept;
 			std::string connection;
 		};
+		std::map<std::string, std::string> headerMap;
+		std::vector<std::string> headerAcceptedFields;
+		std::vector<std::string> partsAcceptedFields;
+		std::string boundary;
 		std::vector<std::string> acceptedFields;
 		bool isValidMethod(const std::string& method);
 		bool isValidResource(const std::string& resource);
 		bool isValidHttpVersion(const std::string& httpVersion);
 		void parseHttpRequest(const std::string& requestLine, HttpRequest& httpRequest);
-		void parseHeaderBlock(std::fstream &requestFile, std::string &line);
-		bool isAcceptedField(std::string field);
-		void parseHeaderLine(const std::string& header, std::map<std::string, std::string>& headerMap);
-		void printHeaderMap(std::map<std::string, std::string> headerMap);
+		bool isAcceptedField(std::string field, std::vector<std::string> acceptedFields);
+		void parseHeaderLine(const std::string& header, std::map<std::string, std::string> &headerMap, std::vector<std::string> acceptedFields);
+		void parseHeaderBlock(std::fstream &requestFile, std::string &line, std::map<std::string, std::string> &headerMap, std::vector<std::string> acceptedFields);
+		void parseHeaderBlockFromString(std::string& inputString, std::string& line, std::map<std::string, std::string>& headerMap, std::vector<std::string> acceptedFields);
+		void parseContentTypeValue();
+		void parseBody(std::fstream &requestFile);
+		void toLowerCase(std::string& str);
+		void splitString(const std::string& input, const std::string& delimiter, std::vector<std::string>& output);
+		void trimWhitespace(std::string& str);
+		bool startsWith(const std::string& fullString, const std::string& start);
+		void removeFromStringStart(std::string& fullString, const std::string& prefix);
+		void printHttpRequest(HttpRequest& httpRequest);
+		void printMap(std::map<std::string, std::string> map);
 	public:
 		PostUploadRequestParser();
 		~PostUploadRequestParser();
