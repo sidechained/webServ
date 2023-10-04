9184d762ee8e144cc254ae092eae9b79e727f413
Graham <graham.r.booth@gmail.com>
2023-10-04 14:17:46 +0200

    added port and ip validation to ConfigFileParser, along with test configs

--- server_sandbox/Parser/ConfigFileParser.cpp
+++ server_sandbox/Parser/ConfigFileParser.cpp
@@ -13,7 +13,10 @@ ConfigFileParser::ConfigFileParser(std::string filename) {
 	}
 	numServers = serverConfigs.size();
 	configFile.close();
+	validateIPs();
 	validatePorts();
+	print();
+
 }
 
 ConfigFileParser::ConfigFileParser(const ConfigFileParser &o)
@@ -33,32 +36,55 @@ ConfigFileParser::~ConfigFileParser()
 	
 }
 
+// check for duplicate IPs across server configs
+void ConfigFileParser::validateIPs() {
+    std::vector<std::string> seenIPs;  // Vector to store seen port numbers
+	for (unsigned int i = 0; i < serverConfigs.size(); i++)
+	{
+		std::string ip = serverConfigs[i].ip;
+		bool isDuplicate = false;
+		for (std::vector<std::string>::iterator seenIt = seenIPs.begin(); seenIt != seenIPs.end(); ++seenIt) {
+			if (*seenIt == ip) {
+				isDuplicate = true;
+				break;
+			}
+		}
+		if (isDuplicate) {
+			std::cout << "Duplicate port number '" << ip << "' found in server config, exiting" << std::endl;
+			exit(EXIT_FAILURE);
+		} 
+		seenIPs.push_back(ip);
+	}	
+}
+
+// check for and remove duplicate ports in a server config
 void ConfigFileParser::validatePorts() {
     std::vector<int> seenPortNumbers;  // Vector to store seen port numbers
-    for (std::vector<PortConfig>::iterator it = serverConfigs[0].portConfigs.begin(); it != serverConfigs[0].portConfigs.end(); /* increment inside loop */) {
-        int portNumber = it->number;
+	for (unsigned int i = 0; i < serverConfigs.size(); i++)
+	{
+		for (std::vector<PortConfig>::iterator it = serverConfigs[i].portConfigs.begin(); it != serverConfigs[i].portConfigs.end(); /* increment inside loop */) {
+			int portNumber = it->number;
+			// Check if the port number has been seen before
+			bool isDuplicate = false;
+			for (std::vector<int>::iterator seenIt = seenPortNumbers.begin(); seenIt != seenPortNumbers.end(); ++seenIt) {
+				if (*seenIt == portNumber) {
+					isDuplicate = true;
+					break;
+				}
+			}
 
-		std::cout << "here2" << std::endl;
-        // Check if the port number has been seen before
-        bool isDuplicate = false;
-        for (std::vector<int>::iterator seenIt = seenPortNumbers.begin(); seenIt != seenPortNumbers.end(); ++seenIt) {
-            if (*seenIt == portNumber) {
-                isDuplicate = true;
-                break;
-            }
-        }
-
-        if (isDuplicate) {
-            // Remove duplicate port number by erasing the element and advancing the iterator
-			std::cout << "Duplicate port number " << portNumber << " found in server config, removing..." << std::endl;
-            it = serverConfigs[0].portConfigs.erase(it);
-        } else {
-            // Add the port number to the vector of seen port numbers
-            seenPortNumbers.push_back(portNumber);
-            // Move to the next port config
-            ++it;
-        }
-    }
+			if (isDuplicate) {
+				// Remove duplicate port number by erasing the element and advancing the iterator
+				std::cout << "Duplicate port number " << portNumber << " found in server config, removing..." << std::endl;
+				it = serverConfigs[i].portConfigs.erase(it);
+			} else {
+				// Add the port number to the vector of seen port numbers
+				seenPortNumbers.push_back(portNumber);
+				// Move to the next port config
+				++it;
+			}
+		}
+	}	
 }
 
 void ConfigFileParser::print() const {
