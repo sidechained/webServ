a9dfc8e8d161015caf4f0624156b980f8f71cbd8
Marco Vomiero <mvomiero@student.42berlin.de>
2023-10-06 11:26:20 +0200

    php infinite loop

--- scripts/infinite_loop.php
+++ scripts/infinite_loop.php
@@ -1,10 +1,8 @@
 <?php
 
-/*while (true) {
+while (true) {
     // Your code here
 
     // Optionally, you can add a delay to prevent the loop from consuming too many resources
     sleep(1); // Sleep for 1 second
-}*/
-
-sleep(8);
+}
